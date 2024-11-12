1389d9ac6b7ca954b7673ae4f46b439330ea1971
Cocottinha <lucascottamuniz@hotmail.com>
2024-11-12 19:58:23 -0300

    att

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
-        Profile
+        <h2>Profile</h2>
+        <div>Nome: {{ user.name }}</div>
+        <div>Cargo: {{ user.role }}</div>
     </div>
 </template>
 <script>
-
+export default {
+  data() {
+    return {
+      user: [],
+    };
+  },
+  mounted() {
+    this.user = JSON.parse(localStorage.getItem('currentUser')) || [];
+  },
+};
 </script>
